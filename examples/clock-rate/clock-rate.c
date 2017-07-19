57f75a4fffe7013c70d87f60913ea832610a6fc6
Josh Droney <joshd@commtech-fastcom.com>
2017-07-19 13:34:25 -0500

    Removed unnecessary header files, made port number universal.

--- examples/clock-rate/clock-rate.c
+++ examples/clock-rate/clock-rate.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <Windows.h>
-#include <ntddser.h>
 #include "../../inc/asynccom.h"
 
 int main(void)
--- examples/tutorial/tutorial.c
+++ examples/tutorial/tutorial.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <Windows.h>
-#include <ntddser.h>
 #include "../../inc/asynccom.h"
 
 int main(void)
@@ -14,7 +13,7 @@ int main(void)
     BOOL success;
 
 	/* Open port 0 in a blocking IO mode */
-	h = CreateFile(L"\\\\.\\COM8", GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+	h = CreateFile(L"\\\\.\\COM3", GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
 	if (h == INVALID_HANDLE_VALUE) {
 		fprintf(stderr, "CreateFile failed with %d\n", GetLastError());
 		return EXIT_FAILURE;
@@ -46,6 +45,7 @@ int main(void)
 	ReadFile(h, idata, sizeof(idata), &tmp, NULL);
 
 	fprintf(stdout, "%s\n", idata);
+	CloseHandle(h);
 
 	return 0;
 }
